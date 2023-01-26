744b212b9c4f2473565a15fae2658ab3a88a2b51
Bryson Bloom <brysonbloom@icloud.com>
2023-01-25 21:02:33 -0700

    all draw functions working

--- app.js
+++ app.js
@@ -32,3 +32,68 @@ const containers = [{
     price: 4
 }]
 
+const containersCart = []
+const toppingsCart = []
+const iceCreamCart = []
+
+
+function drawCone() {
+    template = ``
+    containers.forEach(cone => {
+        let coneElem = document.getElementById(`creamContainer`)
+        template +=
+            `<div onclick="addToContainerCart('${cone.name}')" class="card m-3 p-2" style="height: 16rem;">
+            <img src="${cone.image}" alt="">
+            <div class="card-body">${cone.name}</div>
+        </div>`
+        coneElem.innerHTML = template
+
+    })
+}
+function drawCream() {
+    template = ``
+    iceCream.forEach(cream => {
+        let creamElem = document.getElementById(`creamIce`)
+        template +=
+            `<div onclick="addIceCreamToCart('${cream.name}')" class="card m-3 p-2" style="height: 26vh; width: 16vw;">
+            <img src="${cream.image}" alt="">
+            <div class="card-body">${cream.name}</div>
+        </div>`
+        creamElem.innerHTML = template
+
+    })
+}
+
+
+
+function drawToppings() {
+    template = ``
+    toppings.forEach(tops => {
+        let topElem = document.getElementById(`creamTop`)
+        template +=
+            `<div onclick="addToppingToCart('${tops.name}')" class="card m-3 p-2" style="height: 16rem;">
+            <img src="${tops.image}" alt="">
+            <div class="card-body">${tops.name}</div>
+        </div>`
+        topElem.innerHTML = template
+    })
+}
+
+function addToContainerCart(name) {
+    let Cone = containers.find(cone => cone.name == name)
+    console.log(Cone.name)
+
+}
+function addIceCreamToCart(name) {
+    let Cream = iceCream.find(cone => cone.name == name)
+    console.log(Cream.name)
+
+}
+function addToppingToCart(name) {
+    let tops = toppings.find(cone => cone.name == name)
+    console.log(tops.name)
+
+}
+drawCone()
+drawCream()
+drawToppings()
